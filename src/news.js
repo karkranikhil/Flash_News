69ce26d3b2e3e3c218f7aef629e32d3feb695dc7
nikhil karkra <karkra.nikhil@gmail.com>
2018-10-20 21:26:37 +1100

    Update news.js

--- src/news.js
+++ src/news.js
@@ -1,7 +1,7 @@
 
 export async function getNews(country) {
-  let url = country?`https://newsapi.org/v2/top-headlines?language=en&country=${country}&apiKey=b84166fa08d94634a8f0b29d5314afba`:
-  `https://newsapi.org/v2/top-headlines?language=en&apiKey=b84166fa08d94634a8f0b29d5314afba`;
+  let url = country?`https://newsapi.org/v2/top-headlines?language=en&country=${country}&apiKey=<YOUR_API_KEY>`:
+  `https://newsapi.org/v2/top-headlines?language=en&apiKey=<YOUR_API_KEY>`;
 
   let result = await fetch(url).then(response => response.json());
   return result.articles;
